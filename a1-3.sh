f02d3694e5a81c806d6e06daf3e3d62bbe48e8fe
pictcomp <pictsecratory@gmail.com>
2025-05-15 10:08:52 +0530

    Update a1-3.sh

--- a1-3.sh
+++ a1-3.sh
@@ -2,16 +2,13 @@
 
 # Function to find biggest of three numbers
 biggest_of_three() {
-  if [ $# -ne 3 ]; then
-    echo "Usage: $0 a b c"
-    return
-  fi
-  a=$1
-  b=$2
-  c=$3
-  if [ $a -ge $b ] && [ $a -ge $c ]; then
+  read -p "Enter first number: " a
+  read -p "Enter second number: " b
+  read -p "Enter third number: " c
+
+  if [ "$a" -ge "$b" ] && [ "$a" -ge "$c" ]; then
     echo "$a is the biggest"
-  elif [ $b -ge $a ] && [ $b -ge $c ]; then
+  elif [ "$b" -ge "$a" ] && [ "$b" -ge "$c" ]; then
     echo "$b is the biggest"
   else
     echo "$c is the biggest"
@@ -20,13 +17,9 @@ biggest_of_three() {
 
 # Function to reverse a number
 reverse_number() {
-  if [ $# -ne 1 ]; then
-    echo "Usage: $0 number"
-    return
-  fi
-  num=$1
+  read -p "Enter a number to reverse: " num
   rev=0
-  while [ $num -gt 0 ]; do
+  while [ "$num" -gt 0 ]; do
     rem=$((num % 10))
     rev=$((rev * 10 + rem))
     num=$((num / 10))
@@ -36,59 +29,53 @@ reverse_number() {
 
 # Function to print word N times
 print_word_n_times() {
-  if [ $# -ne 2 ]; then
-    echo "Usage: $0 N word"
-    return
-  fi
-  N=$1
-  word=$2
-  for ((i=1; i<=N; i++)); do
+  read -p "Enter number of times to print: " N
+  read -p "Enter word: " word
+
+  for ((i = 1; i <= N; i++)); do
     echo "$word"
   done
 }
 
 # Function to sum digits of a 4-digit number
 sum_digits() {
-  if [ $# -ne 1 ]; then
-    echo "Usage: $0 4-digit-number"
+  read -p "Enter a 4-digit number: " num
+
+  if [ ${#num} -ne 4 ] || ! [[ "$num" =~ ^[0-9]+$ ]]; then
+    echo "Please enter a valid 4-digit number"
     return
   fi
-  num=$1
-  if ! [[ $num =~ ^[0-9]{4}$ ]]; then
-    echo "Please enter a 4-digit number"
-    return
-  fi
+
   sum=0
-  for ((i=0; i<${#num}; i++)); do
+  for ((i = 0; i < ${#num}; i++)); do
     digit=${num:$i:1}
     sum=$((sum + digit))
   done
   echo "Sum of digits: $sum"
 }
 
-# Menu-driven based on first argument
-case $1 in
+# Main Menu
+echo "Select an option:"
+echo "a) Find biggest of three numbers"
+echo "b) Reverse a number"
+echo "c) Print word N times"
+echo "d) Sum of digits of 4-digit number"
+read -p "Enter choice (a/b/c/d): " choice
+
+case "$choice" in
   a)
-    shift
-    biggest_of_three "$@"
+    biggest_of_three
     ;;
   b)
-    shift
-    reverse_number "$@"
+    reverse_number
     ;;
   c)
-    shift
-    print_word_n_times "$@"
+    print_word_n_times
     ;;
   d)
-    shift
-    sum_digits "$@"
+    sum_digits
     ;;
   *)
-    echo "Usage:"
-    echo "$0 a num1 num2 num3      # Find biggest"
-    echo "$0 b number              # Reverse number"
-    echo "$0 c N word              # Print word N times"
-    echo "$0 d 4-digit-number      # Sum of digits"
+    echo "Invalid option. Please select a, b, c, or d."
     ;;
 esac
